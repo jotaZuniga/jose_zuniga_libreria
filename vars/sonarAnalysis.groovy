1c583d9673249645a83543abda835409bd05e074
Jose Zuniga <josemzr@gmail.com>
2023-10-26 16:52:07 -0600

    Update sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -4,13 +4,13 @@ def call(String name, String dayOfWeek) {
   sh "echo Hello ${name}. Today is ${dayOfWeek}."
 }
 
-def sonarEnv(serverName) {
+def sonarEnv(String serverName) {
   withSonarQubeEnv(serverName) {
     sh "echo Server loaded"
   }
 }
 
-def waitGate(time: 5, abortPipeline: false) {
+def waitGate(int time: 5, Boolean abortPipeline: false) {
   timeout(time: time, unit: 'MINUTES') {
     waitForQualityGate abortPipeline: abortPipeline
   }
